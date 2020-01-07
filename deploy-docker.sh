4a24e12e7d2226307c8a84cca377b48766e3e3a8
Chris Casano <chriscasano@Chriss-MBP-2.hq.crdb.io>
2020-01-07 10:07:54 -0500

    Revert "updated admin urls and used statement.type in nifi flow" didn't need statement.type This reverts commit fe42adf67d00e06f1038c74a96b0b2f587ea9acb.

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -18,21 +18,20 @@ conserv=$(curl http://localhost:9095/nifi-api/flow/process-groups/$rootpg/contro
 ### curl - Create statement to enable all Controller Services
 
 echo ""
-echo "Go to the NiFi UI (http://localhost:9095/nifi) and make sure all of the Controller Services are running and start your flow."
-read -p "...Did you update the Controller Services and start the flow processors?" cs
+echo "Go to the NiFi UI (http://localhost:9095/nifi) and make sure all of the Controller Services are running."
+read -p "...Did you update the Controller Services?" cs
 
 echo -e "*******************************"
 echo -e "Running Bank Demo"
 echo -e "*******************************"
 docker-compose exec -T roach-0 ./cockroach workload init bank "postgresql://roach-0:26257/bank?sslmode=disable"
-docker-compose exec -T roach-0 ./cockroach sql --insecure --host roach-0 --echo-sql < sql/bank-docker.sql
+docker-compose exec -T roach-0 ./cockroach sql --insecure --host roach-0 --echo-sql < sql/bank-docker_KEY.sql
 docker-compose exec -T roach-0 ./cockroach workload run bank --duration 10s "postgresql://roach-0:26257/bank?sslmode=disable"
 
 # Output IPs
 echo -e "******** Connect **************"
 echo -e "Kafka Broker: localhost:9092 "
-echo -e "NiFi UI: http://localhost:9095/nifi"
-echo -e "Cockroach Admin UI: http://localhost:8090"
+echo -e "NiFi UI: http://localhost:9090/nifi"
 echo -e "SQL (local crdb): cockroach sql --insecure --host localhost --port 5432 --database cis"
 echo -e "SQL (no local crdb): docker exec -it roach-0 /cockroach/cockroach.sh sql --insecure --host haproxy --port 5432 --database cis"
 echo -e "*******************************"
